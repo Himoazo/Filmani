374a112400e185ffad3e4330a501e50746d3d9f5
Hoomam <hoaz1600@student.miun.se>
2025-03-12 18:47:04 +0100

    Login

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -1,13 +1,28 @@
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../Context/AuthContext"
+import { Link } from "react-router-dom";
+
 
 const HeaderComponent = () => {
+  const { user, logout } = useAuth();
   return (
     <header>
+      <Link to="/"><h1 className="text-2xl font-bold tracking-wide mb-2 sm:mb-0 sm:mr-4">LOGO</h1></Link>
         <nav>
             <ul>
               <li> <NavLink to="/">Start</NavLink> </li>
               <li><NavLink to="/reviews">Recesnssioner</NavLink></li>
-              <li><NavLink to="/login">Logga in</NavLink></li>    
+              <li>
+              {!user ? (
+                <NavLink to="/login" className="text-white hover:text-blue-300 transition-colors">
+                  Logga in
+                </NavLink>
+              ) : (
+                <button onClick={logout} className="text-white hover:text-red-300 transition-colors">
+                  Logga ut
+                </button>
+              )}
+              </li>    
             </ul>      
         </nav>
     </header>
